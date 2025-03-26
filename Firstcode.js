1203f5899242c66d2f53f26868f7a6c051981f4c
Sumedhk2002 <sumedhk@heaptrace.com>
2025-03-26 19:53:12 +0530

    add 3 lines of code

---
+++ Firstcode.js
@@ -0,0 +1,14 @@
+let firstName = "Sumedh";
+let lastName = "Kulkarni";
+let age = 23;
+let id = 101;
+let organization = "Heaptrace";
+let JobLocation = "pune"
+
+console.log(`Name is ${firstName}`);
+console.log(`Last Name is ${lastName}`);
+console.log(`Age is ${age}`);
+
+// console.log(`ID is ${id}`);
+// console.log(`organization is ${organization}`);
+// console.log(`JobLocation is ${JobLocation}`);
